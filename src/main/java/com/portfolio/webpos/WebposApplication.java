ffe3f02e171a05d4f55eac644a8149dab4b96ccb
Myeongkook Park <myeongkuk@hotmail.com>
2021-11-29 22:56:41 +0900

    환경설정

---
+++ src/main/java/com/portfolio/webpos/WebposApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.webpos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebposApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/webpos/WebposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.webpos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
