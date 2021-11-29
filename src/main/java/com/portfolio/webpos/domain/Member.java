79c285925dfbdfe5f96946b1c5e6a3869fee975e
Myeongkook Park <myeongkuk@hotmail.com>
2021-11-29 23:33:10 +0900

    11/29 Local DB setting

---
+++ src/main/java/com/portfolio/webpos/domain/Member.java
@@ -0,0 +1,26 @@
+package com.portfolio.webpos.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String email;
+    private String password;
+
+    public Member(){}
+
+    public Member(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+}
